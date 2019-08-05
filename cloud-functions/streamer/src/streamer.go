c57ec70c8aea31275874b1d9026c232a4d8913aa
Trellist <info@trellist.com>
2019-08-05 13:21:59 -0400

    test new content type detection

--- cloud-functions/streamer/src/streamer.go
+++ cloud-functions/streamer/src/streamer.go
@@ -374,23 +374,6 @@ func getNERentry(structResponse NERresponse) NERentry {
 	return nerEntry
 }
 
-func GetFileContentType(out *os.File) (string, error) {
-
-	// Only the first 512 bytes are used to sniff the content type.
-	buffer := make([]byte, 512)
-
-	_, err := out.Read(buffer)
-	if err != nil {
-		return "", err
-	}
-
-	// Use the net/http package's handy DectectContentType function. Always returns a valid
-	// content-type by returning "application/octet-stream" if no others seemed to match.
-	contentType := http.DetectContentType(buffer)
-
-	return contentType, nil
-}
-
 // FileStreamer is the main function
 func FileStreamer(ctx context.Context, e GCSEvent) error {
 	if err := profiler.Start(profiler.Config{
@@ -427,10 +410,7 @@ func FileStreamer(ctx context.Context, e GCSEvent) error {
 		return nil
 	}
 
-	contentType, err := GetFileContentType(f)
-	if err != nil {
-		panic(err)
-	}
+	contentType := http.DetectContentType(slurp)
 	log.Printf("http detected file type as %v", contentType)
 
 	fileKind, _ := filetype.Match(slurp)
