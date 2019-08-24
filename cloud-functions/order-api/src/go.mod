a77014faaf470d84e25c812706ff1dc501957ede
Trellist <info@trellist.com>
2019-08-23 20:22:25 -0400

    add order api

---
+++ cloud-functions/order-api/src/go.mod
@@ -0,0 +1,8 @@
+module orderapi
+
+go 1.12
+
+require (
+	github.com/elastic/go-elasticsearch/v8 v8.0.0-20190731061900-ea052088db25
+	github.com/google/uuid v1.1.1
+)
---
+++ cloud-functions/order-api/src/order-api.go
@@ -0,0 +1,46 @@
+package orderapi
+
+import (
+	"context"
+	"io/ioutil"
+	"log"
+	"net/http"
+
+	"github.com/elastic/go-elasticsearch/v8"
+	"github.com/elastic/go-elasticsearch/v8/esapi"
+	"github.com/google/uuid"
+)
+
+func HelloWorld(w http.ResponseWriter, r *http.Request) {
+	ctx := context.Background()
+	cfg := elasticsearch.Config{
+		Addresses: []string{
+			"http://104.198.136.122:9200",
+		},
+		Username: "elastic",
+		Password: "TsLv8BtM",
+	}
+	es, err := elasticsearch.NewClient(cfg)
+	docID := uuid.New().String()
+	req := esapi.IndexRequest{
+		Index:        "order",
+		DocumentType: "record",
+		DocumentID:   docID,
+		Body:         r.Body,
+		Refresh:      "true",
+	}
+
+	res, err := req.Do(ctx, es)
+	if err != nil {
+		log.Fatalf("Error getting response: %s", err)
+	}
+	defer res.Body.Close()
+
+	if res.IsError() {
+		resB, _ := ioutil.ReadAll(res.Body)
+		log.Printf("[%s] Error indexing document ID=%v, Message=%v", res.Status(), docID, string(resB))
+	} else {
+		resB, _ := ioutil.ReadAll(res.Body)
+		log.Printf("[%s] document ID=%v, Message=%v", res.Status(), docID, string(resB))
+	}
+}
