eba4864065e05d6e6baf7b0952f6238bd8461d4e
Jie Yang <jyang@trellist.com>
2019-09-30 13:53:40 -0400

    add row logging

--- cloud-functions/streamer/src/streamer.go
+++ cloud-functions/streamer/src/streamer.go
@@ -438,6 +438,7 @@ func FileStreamer(ctx context.Context, e GCSEvent) error {
 		// records = csvRecords
 	}
 	log.Printf("found %v rows in file", len(allrows))
+	log.Printf("first 10 rows %v", allrows[0:9])
 	// now scan through records
 	var maxColumns int
 	var maxColumnRowAt int
