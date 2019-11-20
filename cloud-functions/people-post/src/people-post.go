eb22cdb3244552791a973c6394148ef2372a263e
Marcus Tewksbury <mtewksbury@Setups-MacBook-Pro.local>
2019-11-20 13:53:14 -0500

    adding name test

--- cloud-functions/people-post/src/people-post.go
+++ cloud-functions/people-post/src/people-post.go
@@ -521,6 +521,12 @@ func PostProcessPeople(ctx context.Context, m PubSubMessage) error {
 			} else if column.PeopleVER.IS_COUNTRY {
 				if dev { log.Printf("ERR COUNTRY: %v %v %v", column.Name, column.Value, input.Signature.EventID) }
 				SetMkField(&mkOutput, "COUNTRY", column.Value, column.Name)
+			} else if column.PeopleERR.ContainsFirstName == 1 {
+				if dev { log.Printf("FName with loose ERR: %v %v %v", column.Name, column.Value, input.Signature.EventID) }
+				SetMkField(&mkOutput, "FNAME", column.Value, column.Name)
+			} else if column.PeopleERR.ContainsLastName == 1 {
+				if dev { log.Printf("LName with loose ERR: %v %v %v", column.Name, column.Value, input.Signature.EventID) }
+				SetMkField(&mkOutput, "LNAME", column.Value, column.Name)
 			}
 
 		} else if column.PeopleERR.ContainsRole == 1 {
