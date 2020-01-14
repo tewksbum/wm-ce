07fbf5080c4710e0f4d3d7a7df44eb5703d0df65
Marcus Tewksbury <mtewksbury@Marcuss-MacBook-Pro.local>
2020-01-14 11:28:15 -0500

    couple pre

--- cloud-functions/utilities/purge/src/purge_test.go
+++ cloud-functions/utilities/purge/src/purge_test.go
@@ -219,7 +219,7 @@ func TestProcessRequestXXX(t *testing.T) {
 		"targetType": "datastore",
 		"targetLevel": "kind",
 		"operation": "delete",
-		"targetSelection": "dev-eiu-nrhh",
+		"targetSelection": "dev-amu-fheg",
 		"targetSubSelection": ""
 	}`
 
