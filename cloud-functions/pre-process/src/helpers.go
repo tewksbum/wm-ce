ef62e986dd9d85271dc6da27b4de87a707222fb4
Pabla Aquino <pabla.aqui@gmail.com>
2020-05-04 12:08:05 -0400

    add acd options for uil

--- cloud-functions/pre-process/src/helpers.go
+++ cloud-functions/pre-process/src/helpers.go
@@ -135,7 +135,7 @@ func GetPeopleERR(column string) PeopleERR {
 		err.ZipCode = 1
 	case "county":
 		err.County = 1
-	case "country", "country (blank for us)", "home_country", "home country", "address country", "address country name", "pa nation", "nation", "country description", "parent nation", "active country", "justatest":
+	case "country", "country (blank for us)", "home_country", "home country", "address country", "address country name", "pa nation", "nation", "country description", "parent nation", "active country", "country description 4":
 		err.Country = 1
 	case "address", "student address", "parent address", "home address", "permanent address":
 		err.FullAddress = 1
