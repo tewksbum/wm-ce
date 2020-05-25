0aeb587b57f3396727453a62e01c1e864a757e61
pbaqui <pabla.aqui@gmail.com>
2020-05-25 14:34:00 -0400

    Feature/prefix (#676)
    * add prefix edit

--- cloud-functions/file-processor/src/file-processor.go
+++ cloud-functions/file-processor/src/file-processor.go
@@ -564,6 +564,20 @@ func ProcessFile(ctx context.Context, m PubSubMessage) error {
 			log.Printf("Header row is %v", headers)
 			records = allrows[maxColumnRowAt+1:]
 
+			//Clean Prefix
+			for i := range headers {
+				if len(headers[i]) > 0 && reStartsWithPrefix.MatchString(headers[i]) {
+					log.Printf("The header column starts with a prefix: %v", headers[i])
+					result := reStartsWithPrefix.FindStringSubmatch(headers[i])
+					if len(result) >= 3 {
+						headers[i] = result[2]
+					}
+					log.Printf("The header column starts with a prefix result: %v", headers[i])
+				}
+			}
+
+			log.Printf("headersTEST: %v", headers)
+
 			// attempt to detect if file has no header
 			// a. if the header has any column that contains same value that is not blank as the rest of the rows
 			// b. if the header contains any column that starts with a number
