52ade0fe6f91e6e9cfc22c93468a1082f633f6f8
pbaqui <pabla.aqui@gmail.com>
2020-06-02 16:24:37 -0400

    Pabla/fixes (#725)
    * adding status message

--- cloud-functions/file-processor/src/file-processor.go
+++ cloud-functions/file-processor/src/file-processor.go
@@ -403,6 +403,17 @@ func ProcessFile(ctx context.Context, m PubSubMessage) error {
 					}
 					publishReport(&report, cfName)
 
+					input.EventData["message"] = "Unable to parse xlsx"
+					input.EventData["status"] = "Error"
+					statusJSON, _ := json.Marshal(input)
+					psresult := status.Publish(ctx, &pubsub.Message{
+						Data: statusJSON,
+					})
+					_, pserr := psresult.Get(ctx)
+					if pserr != nil {
+						log.Fatalf("%v Could not pub status to pubsub: %v", input.Signature.EventID, err)
+					}
+
 					log.Printf("ERROR unable to parse xlsx: %v", err)
 					return fmt.Errorf("unable to parse xlsx: %v", err)
 				}
