ad9619fd1c4d565fd7476372d61233b0bc3a8283
Jie Yang <jyang@mini.local>
2020-06-15 17:52:44 -0400

    remove excessive logging

--- cloud-functions/file-processor/src/file-processor.go
+++ cloud-functions/file-processor/src/file-processor.go
@@ -670,7 +670,7 @@ func ProcessFile(ctx context.Context, m PubSubMessage) error {
 				headerlessTest1 = true
 			}
 			if headerlessTest1 {
-				log.Printf("%v is headerless (header row value is repeated in records %v times), stop processing", input.Signature.EventID, repeatedValueCount)
+				// log.Printf("%v is headerless (header row value is repeated in records %v times), stop processing", input.Signature.EventID, repeatedValueCount)
 
 				report := FileReport{
 					ID:          input.Signature.EventID,
