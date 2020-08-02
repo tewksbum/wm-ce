28d7c1db3fac1154cd10ecf43ad8d51698cfdd71
Jie Yang <jyang@mini.local>
2020-08-02 12:53:46 -0400

    hourly

---
+++ cloud-functions/dsr-orders-list-hourly/src/main_test.go
@@ -0,0 +1,13 @@
+package dsrorderlist
+
+import (
+	"context"
+	"testing"
+
+	"cloud.google.com/go/pubsub"
+)
+
+func TestRun(t *testing.T) {
+	var message pubsub.Message
+	Run(context.Background(), &message)
+}
