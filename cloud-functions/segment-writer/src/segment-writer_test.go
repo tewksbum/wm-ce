21a9dbd0092e3863ad873c637039b4d77db87228
Jie Yang <jyang@mini.local>
2020-11-13 12:14:03 -0500

    segment-writer update

--- cloud-functions/segment-writer/src/segment-writer_test.go
+++ cloud-functions/segment-writer/src/segment-writer_test.go
@@ -1,11 +1,12 @@
 package segmentwriter
 
 import (
+	"context"
 	"testing"
 )
 
 func TestRequest(t *testing.T) {
 	var message PubSubMessage
 	message.Data = []byte("")
-	//ProcessOutput(context.Background(), message)
+	ProcessOutput(context.Background(), message)
 }
