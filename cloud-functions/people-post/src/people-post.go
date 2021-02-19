deaa11fd201fa0bd07588b2c34e22f9b3302b702
Pabla Aquino <pabla.aqui@gmail.com>
2021-02-19 09:49:52 -0300

    revert changes in fileprocessor and people-post

--- cloud-functions/people-post/src/people-post.go
+++ cloud-functions/people-post/src/people-post.go
@@ -565,7 +565,7 @@ func PostProcessPeople(ctx context.Context, m PubSubMessage) error {
 				searchValue := strings.Replace(search, "'", `''`, -1)
 				querySets := []PeopleSetDS{}
 				if _, err := fs.GetAll(ctx, datastore.NewQuery(DSKindSet).Namespace(dsNameSpace).Filter("search =", searchValue), &querySets); err != nil {
-					log.Printf("Error querying sets: %v", err)
+					log.Fatalf("Error querying sets: %v", err)
 				}
 				log.Printf("Fiber type %v Search %v found %v sets", v.Type, search, len(querySets))
 				for _, s := range querySets {
